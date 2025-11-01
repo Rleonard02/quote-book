611b32a23b6afb6d7f387cddf6909de54c44ce9c
Ryan Leonard <rleonard772@gmail.com>
2025-11-01 12:51:30 -0400

    kaboom baby

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,35 @@
-function App() {
+import React, { useRef, useState } from "react";
+import "./App.css";
+import { JSX } from "react/jsx-runtime";
+
+function App(): JSX.Element {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  const goToEtsy = () => {
+    window.location.href = "https://www.etsy.com/listing/4396593803/me-myself-my-mind-the-first-book-from?ls=r&ref=hp_recent_activity_hub-1&content_source=727f1b926fb92eaafe8ad9cf687a97b8%253ALT150414f7bdcdd5debea0b7f1592fc99872925acd&logging_key=727f1b926fb92eaafe8ad9cf687a97b8%3ALT150414f7bdcdd5debea0b7f1592fc99872925acd";
+  };
+
   return (
-    <div className="app-container" style={{}}>
-      <h1>Welcome to the Henry Kudish Experience</h1>
-      <button
-        onClick={async () => {
-          // try {
-          //   const res = await fetch("/api/create-checkout-session", {
-          //     method: "POST",
-          //     headers: {
-          //       "Content-Type": "application/json",
-          //     },
-          //   });
-          //   const data = await res.json();
-          //   console.log(data);
-          //   if (data.url) window.location.href = data.url;
-          // } catch (err) {
-          //   console.error(err);
-          // }
-        }}
+    <div className="app-root">
+      <video
+        autoPlay
+        muted
+        loop
+        id="myVideo"
+        ref={videoRef}
+        playsInline
       >
-        BUY THE BOOK
-      </button>
+        <source src="flames.mp4" type="video/mp4" />
+        Your browser does not support HTML5 video.
+      </video>
+
+      <div className="content">
+        <h1>Welcome to the Henry Kudish Experience</h1>
+        <button id="myBtn" onClick={goToEtsy}>
+          BUY THE BOOK
+        </button>
+      </div>
     </div>
   );
 }
